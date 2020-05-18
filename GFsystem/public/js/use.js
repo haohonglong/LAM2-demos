11713683c0eec0819bf02dfe9b04c16f73cd9a14
haohonglong <947923503@qq.com>
2020-05-18 09:33:12 +0800

    modify

--- GFsystem/public/js/use.js
+++ GFsystem/public/js/use.js
@@ -2,142 +2,14 @@ LAM.run([window],function(window){
 	'use strict';
 	var System=this;
 	var I= {};
-	I.home={
-		'highcharts':function($dom){
-			$dom.highcharts({
-				credits: {
-					enabled: false
-				},
-				chart: {
-					zoomType: 'xy',
-					marginBottom: 60,
-					marginTop: 60
-				},
-				title: {
-					text: '今日上网电量实时曲线',
-					verticalAlign: 'bottom',
-					floating:true,
-					y:0,
-					style:{
-						fontSize: 18,
-						fontWeight: 'bolder',
-						color: '#495b79'
-					}
-				},
-				subtitle: {
-					text: ''
-				},
-				xAxis: [{
-					categories: ['0:00', '2:00', '3:00', '4:00', '5:00', '6:00',
-						'7:00', '8:00', '9:00', '10:00', '11:00', '12:00'],
-					crosshair: true
-				}],
-				yAxis: [{ // Primary yAxis
-					labels: {
-						format: '{value}',
-						style: {
-							color: '#464646'
-						}
-					}
-
-				}, { // Secondary yAxis
-					title: {
-						text: '发电量',
-						align:'low',
-						style: {
-							color: '#464646'
-						}
-					},
-					labels: {
-						format: '{value}',
-						style: {
-							color: '#464646'
-						}
-					},
-					opposite: true
-				}],
-				tooltip: {
-					shared: true
-				},
-				legend: {
-					layout: 'horizontal',
-					align: 'center',
-					x: 120,
-					verticalAlign: 'top',
-					y: 0,
-					floating: true,
-					backgroundColor: (Highcharts && Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-
-				},
-				series: [{
-					name: '发电量',
-					type: 'column',
-					yAxis: 1,
-					data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-					tooltip: {
-						valueSuffix: ''
-					},
-					color: '#78b03b'
-
-				}, {
-					name: '光照辐射量',
-					type: 'spline',
-					data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
-					tooltip: {
-						valueSuffix: ''
-					},
-					color: '#f79218'
-				}]
-			});
-		}
-
-		
-
-		
-	};
+	I.home={};
 
 
 	I.scroll=function(){
 		$(".scroll").css({'overflow':'hidden'}).niceScroll({cursorcolor:"#54728c"});
 	};
 	
-	I.side=function(){
-		$('ul.nav-list').accordion();
 
-		$('li.nav-toggle > button').on('click',function(e){
-			e.preventDefault();
-			$('.hidden-minibar').toggleClass("hide");
-			$('.site-holder').toggleClass("mini-sidebar");
-			if($('.toggle-left').hasClass('fa-angle-double-left')){ $('.toggle-left').removeClass('fa-angle-double-left').addClass('fa-angle-double-right'); }
-			else { $('.toggle-left').removeClass('fa-angle-double-right').addClass('fa-angle-double-left'); }
-
-
-			if($('.site-holder').hasClass('mini-sidebar'))
-			{
-				$('.sidebar-holder').tooltip({
-					selector: "a",
-					container: "body",
-					placement: "right"
-				});
-				$('li.submenu ul').tooltip('destroy');
-			}
-			else
-			{
-				$('.sidebar-holder').tooltip('destroy');
-			}
-
-
-		});
-
-
-		$('.nav-list a').on('click',function(){
-			$('.nav-list li').removeClass('active');
-			if('iframe' === $(this).attr('target')){
-				$(this).parent().addClass('active');
-
-			}
-		});
-	};
 	
 
 	I.header=function(){
@@ -158,7 +30,8 @@ LAM.run([window],function(window){
 						,url
 						,type
 						,dataType
-						,async){
+						,async
+						,cache){
 		if(!D || !System.isObject()){
 			throw new Error("Warning :缺少重要参数");
 		}
